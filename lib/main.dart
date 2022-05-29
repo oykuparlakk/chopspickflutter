05d1f296a5ab8bd64e46bc322be2862000cd0fa2
oyku <oyku@DESKTOP-73QGJ2T>
2022-05-29 13:51:04 +0300

    user page düzenlendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-
 import 'package:chopspickflutter/pages/login_page.dart';
+import 'package:chopspickflutter/pages/user_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: UserPage(),
     );
   }
 }
